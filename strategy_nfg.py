6621cae848df774fc6c2ffa32337980ca1ca1e31
Verouchis George <GVerouchis@bankofgreece.gr>
2025-08-23 18:16:25 +0300

    NFG at the ready

--- strategy_nfg.py
+++ strategy_nfg.py
@@ -17,9 +17,31 @@ def load_excel_sheets(file_path):
 def clean_df1(df1):
     logger.info("Cleaning first sheet (df1)...")
     try:
-        df1 = df1.iloc[2:]
-        df1.columns = df1.iloc[0]
-        df1 = df1[1:]
+        df1 = df1.iloc[1:]
+        df1.columns = df1.iloc[1]
+        
+        account_entry_row = []
+
+        for idx, col in enumerate(df1.columns):
+            # use index rather than column name to identify first column
+            if idx == 0:
+                account_entry_row.append("ACCOUNT_ENTRY")
+            else:
+                # access by integer location (column index) instead of name
+                val = str(df1.iloc[0, idx]).lower()
+
+                if "receivable" in val:
+                    account_entry_row.append("C")
+                    
+                elif "payable" in val:
+                    account_entry_row.append("D")
+                else:
+                    account_entry_row.append("_Z")
+        # remove last value from list
+        account_entry_row.pop()
+        account_entry_row[0] = "ACCOUNT_ENTRY"
+
+        df1 = df1[2:]
         df1.rename(columns={df1.columns[0]: "Date"}, inplace=True)
         df1 = df1.iloc[1:]
         df1["Date"] = df1["Date"].str.replace("*", "", regex=False)
@@ -40,6 +62,13 @@ def clean_df1(df1):
         logger.success("df1 cleaned successfully.")
         #drop column D995
         df1 = df1.drop(columns=["D995"])
+        # Create a one-row DataFrame for insertion
+        row_df = pd.DataFrame([account_entry_row], columns=df1.columns)
+        # add row at the end
+        df1 = pd.concat([df1, row_df], ignore_index=True)
+
+        
+        print("DF1 FINAL\n",df1.head())
         return df1
     except Exception as e:
         logger.error(f"Error cleaning df1: {e}")
@@ -49,6 +78,21 @@ def clean_df2(df2):
     logger.info("Cleaning second sheet (df2)...")
     try:
         df2 = df2.iloc[1:]
+        account_entry_row = []
+
+        for idx, col in enumerate(df2.columns):
+            # use index rather than column name to identify first column
+            if idx == 0:
+                account_entry_row.append("ACCOUNT_ENTRY")
+            else:
+                val = str(df2.iloc[0, idx]).lower()
+                if "receivable" in val:
+                    account_entry_row.append("C")
+                elif "payable" in val:
+                    account_entry_row.append("D")
+                else:
+                    account_entry_row.append("_Z")
+        account_entry_row[0] = "ACCOUNT_ENTRY"
         df2.columns = df2.iloc[0].str.split("\n").str[0]
         df2 = df2[1:]
         df2 = df2.drop(df2.index[1])
@@ -68,6 +112,13 @@ def clean_df2(df2):
         ]).reset_index(drop=True)
         df2.reset_index(drop=True, inplace=True)
         logger.success("df2 cleaned successfully.")
+        row_df = pd.DataFrame([account_entry_row], columns=df2.columns)
+        # add row at the end
+        df2 = pd.concat([df2, row_df], ignore_index=True)
+
+        
+
+        print("DF2 FINAL\n",df2.head())
         return df2
     except Exception as e:
         logger.error(f"Error cleaning df2: {e}")
@@ -79,8 +130,8 @@ def merge_and_finalize(df1, df2):
         merged_df = pd.merge(df1, df2, on="Date", how='outer', suffixes=('_df1', '_df2'))
         
         # Move the last 2 rows to the beginning
-        last_two_rows = merged_df.tail(2)
-        merged_df = merged_df.iloc[:-2]  # Remove last 2 rows
+        last_two_rows = merged_df.tail(3)
+        merged_df = merged_df.iloc[:-3]  # Remove last 2 rows
         merged_df = pd.concat([last_two_rows, merged_df], ignore_index=True)
         
         # Delete the last row
